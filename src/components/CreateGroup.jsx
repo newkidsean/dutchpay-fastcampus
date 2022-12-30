a317e7b7dee6116685532c220b16054def89468a
newkidsean <cassislagos@gmail.com>
2022-12-30 13:17:21 +0900

    style: 그룹 생성 컴포넌트 css

--- src/components/CreateGroup.jsx
+++ src/components/CreateGroup.jsx
@@ -3,6 +3,7 @@ import CenteredOverlayForm from "./CenteredOverlayForm";
 import { useRecoilState } from 'recoil';
 import { groupNameState } from "../state/groupName";
 import { useState } from "react";
+import styled from 'styled-components'
 
 const CreateGroup = () => {
   const [groupName, setGroupName] = useRecoilState(groupNameState);
@@ -23,32 +24,62 @@ const CreateGroup = () => {
   };
 
   return (
-    <div>
-      <h1>Dutch Pay</h1>
+    <CenteredOverlayForm>
       <Container>
         <Form noValidate validated={validated} onSubmit={handleSubmit}>
-          <Row>
-            <h1>먼저, 더치 페이 할 그룹의 이름을 정해볼까요?</h1>
-          </Row>
-          <Row>
-            <Form.Group controlId="validataionGroupName">
-              <Form.Control
-                type="text"
-                required
-                placeholder="2022 제주도 여행"
-                onChange={(event) => setGroupName(event.target.value)}
-              />
-              <Form.Control.Feedback type="invalid" data-valid={validGroupName}>그룹 이름을 입력해 주세요</Form.Control.Feedback>
-            </Form.Group>
-          </Row>
-          <Button type="submit">저장</Button>
-          <Row>
-          </Row>
+          <StyledRow>
+
+            <Row className="align-items-start">
+              <StyledH2>먼저, 더치 페이 할 그룹의 이름을 정해볼까요?</StyledH2>
+            </Row>
+
+            <Row className="align-items-center">
+              <Form.Group controlId="validataionGroupName">
+                <Form.Control
+                  type="text"
+                  required
+                  placeholder="2022 제주도 여행"
+                  onChange={(event) => setGroupName(event.target.value)}
+                />
+                <Form.Control.Feedback type="invalid" data-valid={validGroupName}>그룹 이름을 입력해 주세요</Form.Control.Feedback>
+              </Form.Group>
+            </Row>
+
+            <Row className="align-items-end">
+              <SubmitButton>저장</SubmitButton>
+            </Row>
+          </StyledRow>
         </Form>
       </Container>
-      {/* <CenteredOverlayForm /> */}
-    </div>
+    </CenteredOverlayForm>
   )
 };
 
 export default CreateGroup;
+
+const StyledRow = styled(Row)`
+  height: 60vh;
+  justify-content: center;
+  align-items: center;
+`;
+
+const SubmitButton = styled(Button).attrs({
+  type: 'submit',
+})`
+background-color: #6610F2;
+border-radius: 8px;
+border: none;
+
+&:hover {
+  background-color: #6610F2;
+  filter: brightness(80%);
+}
+`;
+
+const StyledH2 = styled.h2`
+  text-align: right;
+  overflow-wrap: break-word;
+  word-break: keep-all;
+  font-weight: 700;
+  line-height: 35px;
+`
---
+++ src/components/shared/OverlayWrapper.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const OverlayWrapper = ({ children }) => {
+  return (
+    <StyledContainer>{children}</StyledContainer>
+  )
+}
+
+export default OverlayWrapper;
+
+const StyledContainer = styled.div`
+  background-color: #FFFFFF;
+  border-radius: 15px;
+  min-height: ${(props) => props.minHeight || '0'};
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  padding: ${(props) => props.padding || '5vw'};
+`;
